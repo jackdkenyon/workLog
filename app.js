c8f61fdb2094024e80285075ce470a8385648583
jackdkenyon <43279364+jackdkenyon@users.noreply.github.com>
2018-09-19 15:41:00 +0000

    Update app.js

--- app.js
+++ app.js
@@ -4,24 +4,10 @@ var cookieParser = require('cookie-parser');
 var session = require('express-session');
 var mongoStore = require('connect-mongo')(session);
 var mongoose = require('mongoose');
-require('./models/users_model.js');　
-
-var conf = {
-    db: {
-        db: 'worklog',
-        host: 'localhost',
-        port: 27017,  // optional, default: 27017
-        //username: 'admin', // optional
-        //password: 'secret', // optional
-        //collection: 'mySessions' // optional, default: sessions
-         },
-    //secret: '076ee61d63aa10a125ea872411e433b9'
-    };
-
-var db = mongoose.connect('mongodb://localhost:27017/worklog');
-
+require('./models/users_model.js');
+var argv = require('optimist').argv;
+mongoose.connect('mongodb://10.142.0.2:80/worklog');
 var app = express();
-
 app.engine('.html', require('ejs').__express);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'html');
@@ -30,15 +16,16 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(session({
     secret: 'SECRET',
-   cookie: {maxAge: 60*60*1000},
-   saveUninitialized: false,
-   resave: false,
-   // url: 'mongodb://localhost/worklog',
-   store: new mongoStore(conf.db)
-
-  }));
-
+    cookie: {maxAge: 60*60*1000},
+    saveUninitialized: false,
+     resave: false,
+    url: 'mongodb://10.142.0.2:80/worklog',
+    store: new mongoStore({
+        url: 'mongodb://10.142.0.2:80/worklog',
+        // db: mongoose.connection.db,
+        touchAfter: 24 * 3600 
+         })
+     }));
 require('./routes')(app);
-
-app.listen(80, '10.142.0.2');
-console.log("listening on 80");
+app.listen(80, '10.142.0.3');
+console.log("Listening on port 80");
