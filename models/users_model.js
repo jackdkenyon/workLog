1e18dc9a6a1394268dec790538f0a46d7fb4f212
jackdkenyon <43279364+jackdkenyon@users.noreply.github.com>
2018-10-04 00:02:33 +0000

    Add files via upload

---
+++ models/users_model.js
@@ -0,0 +1,20 @@
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+
+var UserSchema = new Schema({    
+    username: { type: String, unique: true },
+    hashed_password: String      
+});
+mongoose.model('User', UserSchema);
+
+
+var WorkLogSchema = new Schema({
+    creator_id : {type: Schema.ObjectId, ref: 'User' },
+    date: String,
+    ship: String,
+    tech: String,
+    jsn: String,
+    comments: String,    
+});
+mongoose.model('WorkLog', WorkLogSchema);
