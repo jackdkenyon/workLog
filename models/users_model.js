29773930a902d7703fd8cfb46826daac8eb881da
jackdkenyon <43279364+jackdkenyon@users.noreply.github.com>
2018-11-20 13:12:44 -0500

    Update users_model.js

--- models/users_model.js
+++ models/users_model.js
@@ -1,13 +1,13 @@
 var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
-
-var UserSchema = new Schema({    
+var UserSchema = new Schema({
+    
     username: { type: String, unique: true },
-    hashed_password: String      
+    hashed_password: String    
+     
 });
 mongoose.model('User', UserSchema);
-
 
 var WorkLogSchema = new Schema({
     creator_id : {type: Schema.ObjectId, ref: 'User' },
